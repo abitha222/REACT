8053f25390c6f38d647f19eebf9368fab2e49c2e
abitha222 <125336717+abitha222@users.noreply.github.com>
2023-12-09 14:28:51 +0530

    DAY 1 CLASSWORK

---
+++ REACT/Class Work/helloworld/src/App.js
@@ -0,0 +1,12 @@
+import Sample from './sample.js'
+import './App.css'
+function App(){
+  return (
+    <div>
+      <Sample/>
+      <h1>HELLO WORLD!!</h1>
+    </div>
+
+  );
+}
+export default App
---
+++ REACT/Class Work/helloworld/src/sample.js
@@ -0,0 +1,9 @@
+function Sample()
+{
+    return(
+            <div>
+            <p>this is a sample</p>
+        </div>
+    );
+}
+export default Sample
